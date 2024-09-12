649a6d9fad568dfa66942d14b78ebcd16b147ece
Bob Hendrikx <b.hendrikx@avular.com>
2024-09-12 09:14:24 +0200

    Moved to XACRO for different wheel configurations - Moved to xacro for the URDF description - Added a drive_configuration = {skid_steer_drive, mecanum_drive} parameter to the description launch file. - Changed both right wheel axes to have their z-axis pointing left, making positive rotation around the z-axis correspond to forward motion. - Added mecanum wheel meshes and configuration to the URDF. - Added MecanumDrive and OdometryPublisher gazebo plugin systems to the URDF. - Changes the description topic from /description to /robot/robot_description.

--- origin_one_description/launch/origin_one_description.launch.py
+++ origin_one_description/launch/origin_one_description.launch.py
@@ -9,49 +9,57 @@
 
 from os import path
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
-from launch.substitutions import PathJoinSubstitution
+from launch.actions import DeclareLaunchArgument, OpaqueFunction, SetLaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+from launch_ros.parameter_descriptions import ParameterValue
+import xacro
 
 
 def generate_launch_description():
 
+    # Set the package name
     package_name = "origin_one_description"
-    urdf_file_path = "urdf/origin_one.urdf"
+    xacro_file_path = "urdf/origin_one.urdf.xacro"
 
-    urdf = open(
-        path.join(FindPackageShare(package_name).find(package_name), urdf_file_path)
-    ).read()
+    # Declare the drive_configuration argument
+    arg_drive_configuration = DeclareLaunchArgument(
+        "drive_configuration",
+        default_value="skid_steer_drive",
+        description="The drive configuration of the robot, either skid_steer_drive (which is default) or mecanum_drive.",
+    )
 
+    # Function to create the URDF description from the Xacro file
+    def create_urdf_description(context):
+        xacro_file = path.join(FindPackageShare(package_name).find(package_name), xacro_file_path)
+        drive_configuration = LaunchConfiguration('drive_configuration').perform(context)
+        urdf = xacro.process_file(xacro_file, mappings={"drive_configuration": drive_configuration}).toxml()
+        return [SetLaunchConfiguration('urdf', urdf)]
+
+    # Declare the namespace argument
     arg_ns = DeclareLaunchArgument(
         name="ns",
-        default_value="",
+        default_value="robot",
         description="Namespace",
     )
 
-    urdf_model_path_arg = DeclareLaunchArgument(
-        name="urdf_model",
-        default_value=PathJoinSubstitution(
-            [FindPackageShare(package=package_name), urdf_file_path]
-        ),
-        description="Absolute path to robot urdf file.",
-    )
-
+    # Create the robot_state_publisher node
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
         namespace=LaunchConfiguration("ns"),
-        parameters=[{"robot_description": urdf}],
+        parameters=[{"robot_description": ParameterValue(LaunchConfiguration('urdf'), value_type=str)}],
         respawn=False,
         respawn_delay=1.0,
         remappings=[("/joint_states", "/robot/joint_states")],
     )
 
     ld = LaunchDescription()
+    ld.add_action(arg_drive_configuration)
+    ld.add_action(OpaqueFunction(function=create_urdf_description))
     ld.add_action(arg_ns)
-    ld.add_action(urdf_model_path_arg)
     ld.add_action(robot_state_publisher_node)
+
 
     return ld
